8390655e637b9e2cfb11a6d5c5f00893080af479
im4everok <imizyk09@gmail.com>
2024-02-19 16:26:51 +0200

    added sql connection factory

---
+++ DapperLearn/Services/SqlConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+namespace DapperLearn.Services
+{
+    public class SqlConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SqlConnection Create()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
