7e7d429a7e820aa758ffca4f51ee98432b0b1db3
im4everok <imizyk09@gmail.com>
2024-02-21 22:18:54 +0200

    added order and product entities

---
+++ DapperLearn/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperLearn.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DateOfOrder { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ DapperLearn/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperLearn.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
